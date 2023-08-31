b2a73e794e2d0ec23c5ecda9e66afe1cd965171e
Петко Гавраилов Иванов <stu2001261026@uni-plovdiv.bg>
2023-08-31 19:31:19 +0300

    Initial commit

---
+++ src/test/java/com/petkoivanov/carCatalog/CarCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petkoivanov.carCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
