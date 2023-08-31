34fb7e0f0a6d9647efc810024738247299790133
Петко Гавраилов Иванов <stu2001261026@uni-plovdiv.bg>
2023-08-31 20:12:22 +0300

    02 - fixed car entity implementation02 - fixed car entity    implementation02 - fixed car entity implementation02 - impfixed car    entity implementation02 - fixed car entity implementation02 -    fixed car entity implementation02 - fixed car entity implementation02    - fixed car entity implementation02 - fixed car entity      implementation02 - fixed car entity implementation02 - implefixed      car entity implementation02 - fixed car entity implementation02 -      fixed car entity implementation02 - fixed car entity      implementation02 - fixed car entity implementation02 - implefixed      car entity implementation02 - fixed car entity implementation02 -      fixed car entity implementation02 - fixed car entity      implementation02 - fixed car entity implementation

--- src/main/java/com/petkoivanov/carCatalog/models/entities/Car.java
+++ src/main/java/com/petkoivanov/carCatalog/models/entities/Car.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import java.sql.Date;
 
 @Entity
 @Table(name = "cars")
@@ -20,7 +22,36 @@ import javax.persistence.Table;
 public class Car {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  int id;
+  private int id;
 
+  @Column(name = "vin_number")
+  private String vinNumber;
 
+  @OneToOne
+  @JoinColumn(name = "model_id")
+  private Model model;
+
+  private double price;
+
+  @Column(name = "reg_date")
+  private Date registrationDate;
+
+  @OneToOne
+  @JoinColumn(name = "transmission_id")
+  private Transmission transmission;
+
+  @OneToOne
+  @JoinColumn(name = "fuel_type_id")
+  private FuelType fuelType;
+
+  public Car(String vinNumber , Model model ,
+             double price , Date registrationDate ,
+             Transmission transmission , FuelType fuelType){
+    this.vinNumber = vinNumber;
+    this.model = model;
+    this.price = price;
+    this.registrationDate = registrationDate;
+    this.transmission = transmission;
+    this.fuelType = fuelType;
+  }
 }
