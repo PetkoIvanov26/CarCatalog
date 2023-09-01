5488488821f721223a570756493c3434fcd6dd01
Петко Гавраилов Иванов <stu2001261026@uni-plovdiv.bg>
2023-09-01 17:33:03 +0300

    fix for car repository

--- src/main/java/com/petkoivanov/carCatalog/repositories/CarRepository.java
+++ src/main/java/com/petkoivanov/carCatalog/repositories/CarRepository.java
@@ -5,20 +5,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface CarRepository extends JpaRepository<Integer , Car> {
   Optional<Car> findByVinNumber(String vinNumber);
   Optional<Car> findByRegDate(LocalDate regDate);
-  Optional<Car> findByModelId(int id);
-  Optional<Car> findByFuelTypeId(int id);
-  Optional<Car> findAllByOrderByPriceAsc();
-  Optional<Car> findAllByOrderByPriceDesc();
-  Optional<Car> findByTransmissionId(int id);
-  Optional<Car> findByTransmissionIdAndModelId(int transmissionId , int modelId);
-  Optional<Car> findByTransmissionIdAndFuelType(int transmissionId , int fuelTypeId);
-  Optional<Car> findByModelIdAndFuelType(int modelId , int fuelTypeId);
-  Optional<Car> findByModelIdAndPriceAndFuelTypeIdAndTransmissionId(int modelId , double price , int fuelType , int transmissionId);
-  Optional<Car> findByModelIdAndFuelTypeIdAndTransmissionIdAndOrderByPriceAsc(int modelId , int fuelId , int transmissionId);
-  Optional<Car> findByModelIdAndFuelTypeIdAndTransmissionIdAndOrderByPriceDesc(int modelId , int fuelId , int transmissionId);
+  List<Car> findAllByModelId(int id);
+  List<Car> findAllByFuelTypeId(int id);
+  List<Car> findAllByOrderByPriceAsc();
+  List<Car> findAllByOrderByPriceDesc();
+  List<Car> findAllByTransmissionId(int id);
+  List<Car> findAllByTransmissionIdAndModelId(int transmissionId , int modelId);
+  List<Car> findAllByTransmissionIdAndFuelType(int transmissionId , int fuelTypeId);
+  List<Car> findAllByModelIdAndFuelType(int modelId , int fuelTypeId);
+  List<Car> findByModelIdAndFuelTypeIdAndTransmissionIdAndPriceGreaterThan(int modelId , double price , int fuelType , int transmissionId);
+  List<Car> findByModelIdAndFuelTypeIdAndTransmissionIdAndPriceLessThan(int modelId , double price , int fuelType , int transmissionId);
+  List<Car> findByModelIdAndFuelTypeIdAndTransmissionIdAndOrderByPriceAsc(int modelId , int fuelId , int transmissionId);
+  List<Car> findByModelIdAndFuelTypeIdAndTransmissionIdAndOrderByPriceDesc(int modelId , int fuelId , int transmissionId);
 }
