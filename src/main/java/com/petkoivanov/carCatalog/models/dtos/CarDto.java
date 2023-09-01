570ddb2bb9545d3649b0dbaa9a9f0d24f2d294ea
Петко Гавраилов Иванов <stu2001261026@uni-plovdiv.bg>
2023-09-01 11:52:43 +0300

    Changed sql.Date to LocalDate in CarDto

--- src/main/java/com/petkoivanov/carCatalog/models/dtos/CarDto.java
+++ src/main/java/com/petkoivanov/carCatalog/models/dtos/CarDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -11,6 +13,7 @@ public class CarDto {
   private String vinNumber;
   private ModelDto model;
   private double price;
+  private LocalDate registrationDate;
   private TransmissionDto transmission;
   private FuelTypeDto fuelType;
 }
