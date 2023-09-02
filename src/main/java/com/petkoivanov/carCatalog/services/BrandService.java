e9b07a8d8f3b7aa5d019356af3b4fdbfa26b170c
Петко Гавраилов Иванов <stu2001261026@uni-plovdiv.bg>
2023-09-02 15:02:04 +0300

    08 - Implemented brand and fuelType services

--- src/main/java/com/petkoivanov/carCatalog/services/BrandService.java
+++ src/main/java/com/petkoivanov/carCatalog/services/BrandService.java
@@ -1,5 +1,104 @@
 package com.petkoivanov.carCatalog.services;
 
+import com.petkoivanov.carCatalog.exceptions.EntityAlreadyExistsException;
+import com.petkoivanov.carCatalog.exceptions.EntityNotFoundException;
+import com.petkoivanov.carCatalog.mappers.BrandMapper;
+import com.petkoivanov.carCatalog.models.dtos.BrandDto;
+import com.petkoivanov.carCatalog.models.entities.Brand;
+import com.petkoivanov.carCatalog.models.requests.BrandRequest;
+import com.petkoivanov.carCatalog.repositories.BrandRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.petkoivanov.carCatalog.utils.ExceptionMessages.BRAND_ALREADY_EXISTS_MESSAGE;
+import static com.petkoivanov.carCatalog.utils.ExceptionMessages.BRAND_NOT_FOUND_MESSAGE;
+
+@Service
 public class BrandService {
 
+  private final static Logger log = LoggerFactory.getLogger(BrandService.class);
+
+  private final BrandMapper brandMapper;
+  private final BrandRepository brandRepository;
+
+  @Autowired
+  public BrandService(BrandMapper brandMapper , BrandRepository brandRepository) {
+    this.brandMapper = brandMapper;
+    this.brandRepository = brandRepository;
+  }
+
+  public Brand addBrand(BrandRequest brandRequest){
+    log.info("An attempt to save a brand in the database");
+
+    brandValidation(brandRequest);
+
+    return brandRepository.save(new Brand(brandRequest.getBrandName()));
+  }
+
+  public List<BrandDto> getAllBrands(){
+    log.info("An attempt to extract all brand from database");
+
+    return brandMapper.mapBrandToBrandDtoList(brandRepository.findAll());
+  }
+
+  public BrandDto getBrandDtoById(int id){
+    log.info(String.format("An attempt to extract brand with id %d from database",id));
+
+    return brandMapper.mapBrandToBrandDto(brandRepository.findById(id).orElseThrow(() -> {
+      log.error(String.format("Exception caught: %s",BRAND_NOT_FOUND_MESSAGE));
+
+      throw new EntityNotFoundException(BRAND_NOT_FOUND_MESSAGE);
+    }));
+  }
+
+  public BrandDto getBrandDtoByName(String name){
+    log.info(String.format("An attempt to extract brand with name %s from database",name));
+
+    return brandMapper.mapBrandToBrandDto(brandRepository.findByName(name).orElseThrow(() -> {
+      log.error(String.format("Exception caught: %s",BRAND_NOT_FOUND_MESSAGE));
+
+      throw new EntityNotFoundException(BRAND_NOT_FOUND_MESSAGE);
+    }));
+  }
+
+  public Brand getBrandById(int id){
+    log.info(String.format("An attempt to extract brand with id %d from database",id));
+
+    return brandRepository.findById(id).orElseThrow(() -> {
+      log.error(String.format("Exception caught: %s",BRAND_NOT_FOUND_MESSAGE));
+
+      throw new EntityNotFoundException(BRAND_NOT_FOUND_MESSAGE);
+    });
+  }
+
+  public BrandDto updateBrand(BrandRequest brandRequest , int brandId){
+    BrandDto brandDto = getBrandDtoById(brandId);
+
+    log.info(String.format("An attempt to update brand with id %d",brandId));
+
+    brandRepository.save(new Brand(brandId , brandRequest.getBrandName()));
+
+    return brandDto;
+  }
+
+  public BrandDto deleteBrand(int brandId){
+    BrandDto brandDto = getBrandDtoById(brandId);
+    brandRepository.deleteById(brandId);
+
+    log.info(String.format("Brand with id %d was deleted from the database", brandId));
+
+    return brandDto;
+  }
+
+  private void brandValidation(BrandRequest brandRequest){
+    brandRepository.findByName(brandRequest.getBrandName()).ifPresent(brand -> {
+      log.error(String.format("Exception caught : %s" , BRAND_ALREADY_EXISTS_MESSAGE));
+
+      throw new EntityAlreadyExistsException(BRAND_ALREADY_EXISTS_MESSAGE);
+    });
+  }
 }
--- src/main/java/com/petkoivanov/carCatalog/services/FuelTypeService.java
+++ src/main/java/com/petkoivanov/carCatalog/services/FuelTypeService.java
@@ -1,5 +1,102 @@
 package com.petkoivanov.carCatalog.services;
 
+import com.petkoivanov.carCatalog.exceptions.EntityAlreadyExistsException;
+import com.petkoivanov.carCatalog.exceptions.EntityNotFoundException;
+import com.petkoivanov.carCatalog.mappers.FuelTypeMapper;
+import com.petkoivanov.carCatalog.models.dtos.FuelTypeDto;
+import com.petkoivanov.carCatalog.models.entities.FuelType;
+import com.petkoivanov.carCatalog.models.requests.FuelTypeRequest;
+import com.petkoivanov.carCatalog.repositories.FuelTypeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.petkoivanov.carCatalog.utils.ExceptionMessages.FUEL_TYPE_ALREADY_EXISTS_MESSAGE;
+import static com.petkoivanov.carCatalog.utils.ExceptionMessages.FUEL_TYPE_NOT_FOUND_MESSAGE;
+
+@Service
 public class FuelTypeService {
 
+  private static final Logger log = LoggerFactory.getLogger(FuelTypeService.class);
+
+  private final FuelTypeMapper fuelTypeMapper;
+  private final FuelTypeRepository fuelTypeRepository;
+
+  @Autowired
+  private FuelTypeService(FuelTypeMapper fuelTypeMapper , FuelTypeRepository fuelTypeRepository){
+    this.fuelTypeMapper = fuelTypeMapper;
+    this.fuelTypeRepository = fuelTypeRepository;
+  }
+
+  public FuelType addFuelType(FuelTypeRequest fuelTypeRequest){
+    log.info("An attempt to save a new fuel type in the database");
+
+    fuelTypeValidation(fuelTypeRequest);
+
+    return fuelTypeRepository.save(new FuelType(fuelTypeRequest.getFuelName()));
+  }
+
+  public List<FuelTypeDto> getAllFuelTypes(){
+    log.info("An attempt to extract all fuel types from database");
+
+    return fuelTypeMapper.mapFuelTypeToFuelDtoTypeList(fuelTypeRepository.findAll());
+  }
+
+  public FuelTypeDto getFuelTypeDtoById(int id){
+    log.info(String.format("An attempt to extract fuel type by id %d from database", id));
+
+    return fuelTypeMapper.mapFuelTypeToFuelTypeDto(fuelTypeRepository.findById(id).orElseThrow(() -> {
+      log.error(String.format("Exception caught: %s",FUEL_TYPE_NOT_FOUND_MESSAGE));
+
+      throw new EntityNotFoundException(FUEL_TYPE_NOT_FOUND_MESSAGE);
+    }));
+  }
+
+  public FuelTypeDto getFuelTypeDtoByName(String name){
+    log.info(String.format("An attempt to extract fuel type with name %s from database", name));
+
+    return fuelTypeMapper.mapFuelTypeToFuelTypeDto(fuelTypeRepository.findByName(name).orElseThrow(() -> {
+      log.error(String.format("Exception caught: %s",FUEL_TYPE_NOT_FOUND_MESSAGE));
+
+      throw new EntityNotFoundException(FUEL_TYPE_NOT_FOUND_MESSAGE);
+    }));
+  }
+
+  public FuelType getFuelTypeById(int id){
+    log.info(String.format("An attempt to extract fuel type by id %d from database", id));
+
+    return fuelTypeRepository.findById(id).orElseThrow(() -> {
+      log.error(String.format("Exception caught: %s",FUEL_TYPE_NOT_FOUND_MESSAGE));
+
+      throw new EntityNotFoundException(FUEL_TYPE_NOT_FOUND_MESSAGE);
+    });
+  }
+
+  public FuelTypeDto updateFuelType(FuelTypeRequest fuelTypeRequest , int fuelTypeId){
+    FuelTypeDto fuelTypeDto = getFuelTypeDtoById(fuelTypeId);
+
+    log.info(String.format("An attempt to update fuel type with id %d", fuelTypeId));
+
+    fuelTypeRepository.save(new FuelType(fuelTypeId,fuelTypeRequest.getFuelName()));
+    return fuelTypeDto;
+  }
+
+  public FuelTypeDto deleteFuelType(int id){
+    FuelTypeDto fuelTypeDto = getFuelTypeDtoById(id);
+
+    fuelTypeRepository.deleteById(id);
+
+    log.info(String.format("Fuel type with id %d was deleted from database", id));
+    return fuelTypeDto;
+  }
+  private void fuelTypeValidation(FuelTypeRequest fuelTypeRequest){
+    fuelTypeRepository.findByName(fuelTypeRequest.getFuelName()).ifPresent(fuelType -> {
+      log.info(String.format("Exception caught: %s",FUEL_TYPE_ALREADY_EXISTS_MESSAGE));
+
+      throw new EntityAlreadyExistsException(FUEL_TYPE_ALREADY_EXISTS_MESSAGE);
+    });
+  }
 }
